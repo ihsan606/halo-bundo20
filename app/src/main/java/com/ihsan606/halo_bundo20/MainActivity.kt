98dd154db0a574ed1d35a8f6d7aea28f7dd77db9
ihsan606 <ihsansyafiul@gmail.com>
2022-12-03 14:36:29 +0700

    Initial commit

---
+++ app/src/main/java/com/ihsan606/halo_bundo20/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ihsan606.halo_bundo20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
